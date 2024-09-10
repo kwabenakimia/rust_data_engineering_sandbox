91aa9117766281661e35f77d35d098eea85fbbc6
kwabenakimia <80727231+kwabenakimia@users.noreply.github.com>
2024-09-10 19:35:24 +0000

    Create devcontainer.js

---
+++ .devcontainer/devcontainer.js
@@ -0,0 +1,13 @@
+{ 
+  "name": "Rust",
+  "image": "mcr.microsoft.com/devcontainers/rust:0-1-bullseye",
+  "customizations": {
+          "vscode": {
+                  "extensions": [
+                          "GitHub.copilot",
+                          "rust-lang.rust-analyzer",
+                          "vadimcn.vscode-lldb",
+                          "vscodevim"
+                    ]
+          }
+  }
